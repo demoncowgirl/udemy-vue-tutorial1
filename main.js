05955e04878e8c5a3c9a6eb08eee8d6526df0a88
Beth Salvatore <bethsalvatore2018@gmail.com>
2018-06-13 11:41:16 -0400

    added exercise2

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 new Vue ({
 	el: '#app',
   	data:{
-    	title: 'Hello World!'
+    	counter1: 0,
+			x: 0,
+			y: 0,
+			counter2: 0
+
     },
 
-    methods:{
-    	// changeTitle: function(event){
-      // 	this.title= event.target.value;
+		methods:{
 
-      sayHello: function(){
-        return 'Hello!';
-      }
-    }
+			increaseCounter1: function(){
+				// increases counter by 1
+				this.counter1++;
+				},
+
+				increaseCounter2: function(step, event){
+					// increase counter by argument passed in html
+					this.counter2 += step;
+					},
+
+			updateCoordinates: function(event){
+				this.x = event.clientX;
+				this.y =event.clientY;
+			},
+
+			dummy: function(event){
+				// stops event from happening in any other elements above it
+				event.stopPropagation();
+			},
+
+			alertMe: function(){
+				alert('Alert!');
+			}
+		}
 });
